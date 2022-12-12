99bb4c7518ff6e8ee58c03038f5845c44ae3f2ae
Marcus Ayers <marcayers27@gmail.com>
2022-12-12 16:14:37 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,23 @@
 Rails.application.routes.draw do
+  #resources :bubbles
   root 'homepage#index'
   get '/feeds' => 'feeds#index'
 
   # USERS
 
+  post '/users' => 'users#create'
+
   # SESSIONS
 
+  get '/authenticated' => 'sessions#authenticated'
+  post '/sessions' => 'sessions#create'
+  delete '/sessions' => 'sessions#destroy'
+
   # TWEETS
+
+  get '/tweets' => 'tweets#index'
+  get '/users/:username/tweets' => 'tweets#index_by_user'
+  post '/tweets/:id' => 'tweets#destroy'
 
   # Redirect all other paths to index page, which will be taken over by AngularJS
   get '*path' => 'homepage#index'
