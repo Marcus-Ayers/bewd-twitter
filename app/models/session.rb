14a258cad22690e0619b69164117452c73821fa1
Marcus Ayers <marcayers27@gmail.com>
2022-12-12 16:05:17 -0500

    session

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,2 +1,9 @@
 class Session < ApplicationRecord
+  before_validation :generate_session_token
+  validates :user_id, presence: true
+  belongs_to :user
+
+  def generate_session_token
+    self.token = SecureRandom.urlsafe_base64
+  end
 end
