3e5f9c4a0fb22f80d710b225b4066fe94a1afc57
Marcus Ayers <marcayers27@gmail.com>
2022-12-12 16:33:47 -0500

    migration files

--- db/migrate/20221212212520_create_sessions.rb
+++ db/migrate/20221212212520_create_sessions.rb
@@ -1,6 +1,10 @@
 class CreateSessions < ActiveRecord::Migration[6.1]
   def change
     create_table :sessions do |t|
+      t.string :token
+      t.string :user_id
+      t.datetime :timestamps
+      t.belongs_to :user, index: true, foreign_key: true
 
       t.timestamps
     end
