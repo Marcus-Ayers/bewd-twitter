6717f3d34478bcb6de3727c7ca5265d3c26ade38
Marcus Ayers <marcayers27@gmail.com>
2022-12-12 16:08:33 -0500

    tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,55 @@
 class TweetsController < ApplicationController
+  def create
+    token = cookies.permanent.signed[:twitter_session_token]
+    session = Session.find_by(token: token)
+
+    user = session.user
+    @tweet = user.tweets.create(message: params[:tweet][:message])
+
+    if @tweet.save!
+      render json: {
+        tweet: {
+          username: @tweet.user.username,
+          message: @tweet.message
+        }
+      }
+    end
+  end
+
+  def destroy
+    token = cookies.permanent.signed[:twitter_session_token]
+    session = Session.find_by(token: token)
+
+    unless session
+      return render json: {
+        success: false
+      }
+    end
+
+    user = session.user
+    tweet = user.tweets.find_by(id: params[:id])
+
+    if tweet.destroy
+      render json: {
+        success: true
+      }
+    else
+      render json: {
+        success: false
+      }
+    end
+  end
+
+  def index
+    @tweets = Tweet.order(created_at: :desc)
+    render 'tweets/index'
+  end
+
+  def index_by_user
+    user = User.find_by(username: params[:username])
+    @tweets = user.tweets
+    if user = true
+      render 'tweets/index'
+    end
+  end
 end
